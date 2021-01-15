22f8d92c902f844e732298aca4fb3b027c93939d
ethanchoi1 <ethanchoi19@gmail.com>
2021-01-15 14:26:45 -0800

    Linked to backend with CORS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import Table from './Table'
 import Form from './Form';
+import axios from 'axios';
 
     class App extends Component {
         state = {
         characters: [],
         }
+        
         handleSubmit = character => {
             this.setState({ characters: [...this.state.characters, character] })
         }
+
         removeCharacter = index => {
             const { characters } = this.state
         
@@ -18,7 +21,21 @@ import Form from './Form';
             }),
             })
         }
+
+        componentDidMount() {
+            axios.get('http://localhost:5000/users')
+            .then(res => {
+            const characters = res.data.users_list;
+            this.setState({ characters });
+            })
+            .catch(function (error) {
+            //Not handling the error. Just logging into the console.
+            console.log(error);
+            });
+        }
         
+ 
+
         render() {
             const { characters } = this.state
         
